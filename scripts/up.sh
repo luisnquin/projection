d5f23c485a5e0699fd91866b2f7094ea0b5b8be2
Luis Quiñones <lpaandres2020@gmail.com>
2024-02-28 06:14:18 -0500

    feat(up.sh): better queue output

--- scripts/up.sh
+++ scripts/up.sh
@@ -12,17 +12,16 @@ main() {
 
     INDEX=0
     for raw_entry in "$buckets_path"/*; do
+        INDEX="$((INDEX + 1))"
         entry="$(basename "$raw_entry")" # profile@bucket
 
         profile_name="${entry%@*}"
         bucket_name=$(echo "$entry" | awk -F '@' '{print $2}')
         files_count=$(find "$raw_entry" -type f | wc -l)
 
-        printf "[%s/%s] \033[38;2;82;127;128m%s\033[0m@%s - syncing %s files...\r" "$INDEX" "$entries_count" "$profile_name" "$bucket_name" "$files_count"
+        printf "[\033[38;2;120;122;128m%s\033[0m/%s] \033[38;2;82;127;128m%s\033[0m@%s - syncing %s files...\r" "$INDEX" "$entries_count" "$profile_name" "$bucket_name" "$files_count"
 
         aws s3 --profile "$profile_name" cp --recursive "$raw_entry" "s3://$bucket_name" --storage-class STANDARD --acl private >/dev/null
-
-        INDEX="$((INDEX + 1))"
 
         printf "[%s/%s] \033[38;2;140;243;245m%s\033[0m@%s - %s files synced up.\n" "$INDEX" "$entries_count" "$profile_name" "$bucket_name" "$files_count"
     done
